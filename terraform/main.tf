1bc34f0dde65e5e0792f88ca8115aecadd4c4262
Snitch <nichola.weatherly@gmail.com>
2022-01-17 09:59:31 +0000

    api gateway mock endpoint

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,12 +6,55 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "snitch-tf-test"
-  acl    = "private"
+resource "aws_api_gateway_rest_api" "api" {
+  name = "health"
+}
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "local"
+resource "aws_api_gateway_resource" "api" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "health"
+}
+
+resource "aws_api_gateway_method" "api" {
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.api.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_integration" "api" {
+  http_method = aws_api_gateway_method.api.http_method
+  resource_id = aws_api_gateway_resource.api.id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  type        = "MOCK"
+}
+
+resource "aws_api_gateway_deployment" "api" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+  triggers = {
+    # NOTE: The configuration below will satisfy ordering considerations,
+    #       but not pick up all future REST API changes. More advanced patterns
+    #       are possible, such as using the filesha1() function against the
+    #       Terraform configuration file(s) or removing the .id references to
+    #       calculate a hash against whole resources. Be aware that using whole
+    #       resources will show a difference after the initial implementation.
+    #       It will stabilize to only change when resources change afterwards.
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.api.id,
+      aws_api_gateway_method.api.id,
+      aws_api_gateway_integration.api.id,
+    ]))
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "api" {
+  deployment_id = aws_api_gateway_deployment.api.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = "dev"
 }
