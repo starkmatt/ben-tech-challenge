ff6bc85ff154707241724ee5c89a92e33ef40aad
nichichi <nichola.weatherly@gmail.com>
2022-01-27 23:53:09 +0000

    changed assign public up to true for ecs service

--- terraform/main.tf
+++ terraform/main.tf
@@ -207,7 +207,7 @@ resource "aws_ecs_service" "ecs"{
   network_configuration {
     security_groups  = [aws_security_group.ecs.id]
     subnets          = [aws_subnet.public1.id,aws_subnet.public2.id]
-    assign_public_ip = false
+    assign_public_ip = true
   }
   
   load_balancer {
